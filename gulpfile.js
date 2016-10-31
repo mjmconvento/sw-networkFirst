ffb58865d1d903b0a418e7e323e3ba0f67871b76
mj <mjmconvento@gmail.com>
2016-11-01 02:59:37 +0800

    added js and manifest to cache

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,12 @@ gulp.task('make-service-worker', function(callback) {
 
     swPrecache.write(path.join(rootDir, 'serviceworker.js'), {
             staticFileGlobs: [rootDir + '/**/*.{html,css,png,jpg,gif,ico}',
-                rootDir + '/js/*.js'
+                rootDir + '/js/*.js',
+                rootDir + '/node_modules/jquery/dist/jquery.js',
+                rootDir + '/manifest.json'
             ],
             runtimeCaching: [{
-                urlPattern: /https:\/\/devcon\.cardistry-arena\.com\/sw-cache\/php_actions\/\w{1,255}\.php/,
+                urlPattern: /http:\/\/localhost\/sw-cache\/php_actions\/\w{1,255}\.php/,
                 handler: 'networkFirst'
             }]
         },
